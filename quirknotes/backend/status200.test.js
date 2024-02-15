a8997e6d31395b0f892cc6a593f46d276fc9b655
JamieHvizdos <114103851+JamieHvizdos@users.noreply.github.com>
2024-02-15 12:41:05 -0500

    Update status200.test.js

--- quirknotes/backend/status200.test.js
+++ quirknotes/backend/status200.test.js
@@ -1,3 +1,8 @@
+test("1+2=3, empty array is empty", () => {
+    expect(1 + 2).toBe(3);
+    expect([].length).toBe(0);
+  });
+
 const SERVER_URL = "http://localhost:4000";
 
 test("/postNote - Post a note", async () => {
@@ -21,8 +26,49 @@ test("/postNote - Post a note", async () => {
   expect(postNoteBody.response).toBe("Note added succesfully.");
 });
 
-test("1+2=3, empty array is empty", () => {
-    expect(1 + 2).toBe(3);
-    expect([].length).toBe(0);
-  });
+test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/deleteNote - Delete a note", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/patchNote - Patch with content and title", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/patchNote - Patch with just title", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/patchNote - Patch with just content", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/deleteAllNotes - Delete one note", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/deleteAllNotes - Delete three notes", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
+test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
+  // Code here
+  expect(false).toBe(true);
+});
+
 
